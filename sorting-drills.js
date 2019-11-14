1e968358a7bc3153c7043153f00bba67cc958148
EJ <ejg132@gmail.com>
2019-11-14 13:37:02 -0800

    finsihed 6

--- sorting-drills.js
+++ sorting-drills.js
@@ -1,4 +1,6 @@
-//1. Understanding merge sort
+const {LinkedList, _Node} = require('./linkedList')
+
+//1.Understanding merge sort
 
 //1a. 
 /* 
@@ -69,3 +71,149 @@ First item is pivot:
 [13, 3, 9, 12, 10, 14] [15, 16, 19, 17]
 [3, 9, 12, 10] [13, 14] [15, 16, 19, 17]
 */
+
+let numbers = [89,30,25,32,72,70,51,42,25,24,53,55,78,50,
+    13,40,48,32,26,2,14,33,45,72,56,44,21,88,27,68,15,62,
+    93,98,73,28,16,46,87,28,65,38,67,16,85,63,23,69,64,91,
+    9,70,81,27,97,82,6,88,3,7,46,13,11,64,76,31,26,38,28,
+    13,17,69,90,1,6,7,64,43,9,73,80,98,46,27,22,87,49,83,6,
+    39,42,51,54,84,34,53,78,40,14,5]
+
+function swap(array, i, j){
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+}
+
+function partition(array, start, end){
+    const pivot = array[end - 1];
+    let j = start;
+    for (let i = start; i < end - 1; i++){
+        if (array[i] <= pivot){
+            swap(array, i, j);
+            j++
+        }
+    }
+    swap(array, end - 1, j);
+    return j;
+}
+
+function qSort(array, start = 0, end = array.length - 1){
+    if (start >= end){
+        return array
+    }
+
+    const middle = partition(array, start, end);
+    array = qSort(array, start, middle);
+    array = qSort(array, middle + 1, end);
+    return array;
+}
+// console.log(qSort(numbers))
+
+// Q4. Merge Sorting
+
+function mergeSort(array){
+    if(array.length <= 1){
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+
+    left = mergeSort(left)
+    right = mergeSort(right)
+    return merge(left, right, array)
+}
+
+function merge(left, right, array){
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let outputIndex = 0;
+    while(leftIndex < left.length && rightIndex < right.length){
+        if(left[leftIndex] < right[rightIndex]){
+            array[outputIndex++] = left[leftIndex++]
+        } else {
+            array[outputIndex++] = right[rightIndex++];
+        }
+    }
+
+    for (let i = leftIndex; i < left.length; i++){
+        array[outputIndex++] = left[i];
+    }
+
+    for(let i = rightIndex; i < right.length; i++){
+        array[outputIndex++] = right[i]
+    }
+
+    return array;
+}
+
+// console.log(mergeSort(numbers))
+
+let list = new LinkedList();
+list.insertFirst(2)
+list.insertLast(21)
+list.insertLast(34)
+list.insertLast(42)
+list.insertLast(12)
+list.insertLast(27)
+list.insertLast(19)
+list.insertLast(33)
+list.insertLast(49)
+list.insertLast(50)
+list.insertLast(9)
+
+function mergeLink(linkedList){
+    let result = linkedList
+    let node = linkedList.head
+    let nextNode = node.next
+    let array = [];
+
+    while(nextNode || node){
+        array.push(node.value)
+        result.remove(node.value)
+
+        if (nextNode === null){
+            break;
+        }
+
+        node = nextNode;
+        nextNode = nextNode.next
+    }
+
+    mergeSort(array)
+
+    for(let i = 0; i < array.length; i++){
+        if(result.head === null){
+            result.insertFirst(array[i])
+        } else {
+            result.insertLast(array[i])
+        }
+    }
+
+    result = JSON.stringify(result)
+
+    return result;
+}
+
+// console.log(mergeLink(list));
+
+let buckets = [87, 42, 15, 2, 79, 50]
+
+function bucketSort(array, max, min){
+
+    let result = [];
+    
+    for(let i = 0; i < max; i++){
+        result[i] = 'EMPTY BUCKET'
+    }
+
+    for(let i = 0; i < array.length; i++){
+        result[array[i] - min] = array[i]
+    }
+
+    return result
+}
+
+// console.log(bucketSort(buckets, 87, 2))
